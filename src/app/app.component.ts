01d192d080a84c3621446ec24a830935f1232cf5
Even Stensberg <evenstensberg@gmail.com>
2024-06-04 13:16:57 +0200

    chore: update prices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class AppComponent implements OnInit {
   ];
   Salads = [
     {
-      price: '175,-',
+      price: '192,-',
       category: 'Salat',
       title:
         'Ost & Skinkesalat - Isbergsalat, ost, skinke, agurk, mais, ananas, tomat, dressing',
@@ -42,7 +42,7 @@ export class AppComponent implements OnInit {
       src: 'assets/salad.png',
     },
     {
-      price: '175,-',
+      price: '192,-',
       category: 'Salat',
       title:
         'Kyllingsalat - Isbergsalat, kylling, agurk, tomat, mais, ananas, dressing',
@@ -50,45 +50,53 @@ export class AppComponent implements OnInit {
       src: 'assets/salad.png',
     },
     {
-      price: '175,-',
+      price: '192,-',
       category: 'Salat',
       title:
         'Gresk salat - Isbergsalat, fetaost, agurk, tomat, mais, oliven, løk, dressing',
       allergies: '1, 3, 7',
       src: 'assets/salad.png',
     },
+    {
+      price: '192,-',
+      category: 'Salat',
+      title:
+        'Kebab salat - Isbergsalat, kebabkjøtt, agurk, mais, tomat, løk, dressing',
+      allergies: '1, 3, 7',
+      src: 'assets/salad.png',
+    },
   ];
   Burgers = [
     {
-      price: '125,-',
+      price: '138,-',
       category: 'Burger',
       title: 'Hamburger 160gr i brød',
       allergies: '1, 3, 7, 11',
       src: 'assets/hamburger.png',
     },
     {
-      price: '145,-',
+      price: '155,-',
       category: 'Burger',
       title: 'Hamburgertallerken 160gr med pommes frites',
       allergies: '1, 3, 7, 11',
       src: 'assets/hamburger.png',
     },
     {
-      price: '180,-',
+      price: '198,-',
       category: 'Burger',
       title: 'Dobbeltburger 2x160gr i brød',
       allergies: '1, 3, 7, 11',
       src: 'assets/hamburger.png',
     },
     {
-      price: '200,-',
+      price: '225,-',
       category: 'Burger',
       title: 'Dobbeltburger tallerken, pommes frites',
       allergies: '1, 3, 7, 11',
       src: 'assets/hamburger.png',
     },
     {
-      price: '145,-',
+      price: '159,-',
       category: 'Burger',
       title:
         'BBQ burger 160gr med bbq-saus, løkringer, ost, salat og hvitløksdressing',
@@ -106,7 +114,7 @@ export class AppComponent implements OnInit {
   ];
   Kebabs = [
     {
-      price: '135,-',
+      price: '149,-',
       category: 'Kebab',
       title:
         'Kebab i pita - kebabkjøtt, isbergsalat, agurk, mais, saus og pitabrød',
@@ -114,14 +122,14 @@ export class AppComponent implements OnInit {
       src: 'assets/kebab.png',
     },
     {
-      price: '180,-',
+      price: '198,-',
       category: 'Kebab',
       title: 'Kebabrull - kebabkjøtt, isbergsalat, agurk, mais, lefse',
       allergies: '1',
       src: 'assets/kebab.png',
     },
     {
-      price: '180,-',
+      price: '198,-',
       category: 'Kebab',
       title: 'Kebabtallerken - kebabkjøtt, salat, pommes frites, saus',
       allergies: '1',
@@ -130,21 +138,21 @@ export class AppComponent implements OnInit {
   ];
   Pastas = [
     {
-      price: '170,-',
+      price: '189,-',
       category: 'Pasta',
       title: 'Bolognese - Spaghetti med kjøttsaus',
       allergies: '1, 9',
       src: 'assets/noodles.png',
     },
     {
-      price: '185,-',
+      price: '199,-',
       category: 'Pasta',
       title: 'Carbonara - Spaghetti med bacon, løk og eggeplomme',
       allergies: '1, 7, 9',
       src: 'assets/noodles.png',
     },
     {
-      price: '195,-',
+      price: '215,-',
       category: 'Pasta',
       title:
         'Husets pasta - Penne, biffkjøtt, paprika, løk, tomat og fløtesaus',
@@ -152,14 +160,14 @@ export class AppComponent implements OnInit {
       src: 'assets/noodles.png',
     },
     {
-      price: '195,-',
+      price: '215,-',
       category: 'Pasta',
       title: 'Con carne - Penne, biffkjøtt, paprika, løk og fløtesaus',
       allergies: '1, 7, 9',
       src: 'assets/noodles.png',
     },
     {
-      price: '195,-',
+      price: '215,-',
       category: 'Pasta',
       title: 'Pasta Indiana - Penne, kylling, paprika, løk og karrisaus',
       allergies: '1, 7, 9',
@@ -168,7 +176,7 @@ export class AppComponent implements OnInit {
   ];
   Fishes = [
     {
-      price: '210,-',
+      price: '229,-',
       category: 'Fisk',
       title:
         'Fish & Chips - panert torskefilet, sylteagurk, remulade og pommes frites',
@@ -176,7 +184,7 @@ export class AppComponent implements OnInit {
       src: 'assets/fishing.png',
     },
     {
-      price: '210,-',
+      price: '229,-',
       category: 'Fisk',
       title: 'Rødspette - panert rødspette, salat, remulade og pommes frites',
       allergies: '1, 3, 4, 10',
@@ -185,14 +193,14 @@ export class AppComponent implements OnInit {
   ];
   Meats = [
     {
-      price: '185,-',
+      price: '199,-',
       category: 'Kjøtt',
       title: 'Løvbifftallerken - salat, pommes frites og bernaise',
       allergies: '3, 9, 10',
       src: 'assets/steak.png',
     },
     {
-      price: '230,-',
+      price: '255,-',
       category: 'Kjøtt',
       title:
         'Biffsnadder - indrefilet, løk, champignons, paprika, pommes frites og bearnaise',
@@ -200,7 +208,7 @@ export class AppComponent implements OnInit {
       src: 'assets/steak.png',
     },
     {
-      price: '230,-',
+      price: '255,-',
       category: 'Kjøtt',
       title:
         'Kyllingsnadder - kyllingfilet, løk, paprika, champignons, pommes frites og bearnaise',
@@ -208,7 +216,7 @@ export class AppComponent implements OnInit {
       src: 'assets/steak.png',
     },
     {
-      price: '240,-',
+      price: '255,-',
       category: 'Kjøtt',
       title:
         'Svinefilet - 2 medallioner svin indrefilet, grillet ananas og bacon',
@@ -216,7 +224,7 @@ export class AppComponent implements OnInit {
       src: 'assets/steak.png',
     },
     {
-      price: '220,-',
+      price: '265,-',
       category: 'Kjøtt',
       title:
         'Kylling Philadephia - kyllingfilet, bacon, salat, Philadephiasaus og pommes frites',
@@ -224,14 +232,14 @@ export class AppComponent implements OnInit {
       src: 'assets/steak.png',
     },
     {
-      price: '300,-',
+      price: '330,-',
       category: 'Kjøtt',
       title: 'Biff indrefilet - salat, pommes frites og valgfri saus',
       allergies: '3, 9, 10',
       src: 'assets/steak.png',
     },
     {
-      price: '300,-',
+      price: '330,-',
       category: 'Kjøtt',
       title:
         'Filetto Monza - okse indrefilet, gorgonzolasaus, champignons, paprika, salat, pommes frites',
@@ -239,7 +247,7 @@ export class AppComponent implements OnInit {
       src: 'assets/steak.png',
     },
     {
-      price: '300,-',
+      price: '330,-',
       category: 'Kjøtt',
       title:
         'Meksikansk biff - okse indrefilet, pommes frites, sterk mexicansk saus og salat',
@@ -247,7 +255,7 @@ export class AppComponent implements OnInit {
       src: 'assets/steak.png',
     },
     {
-      price: '300,-',
+      price: '330,-',
       category: 'Kjøtt',
       title:
         'Black & White - okse indrefilet, svin indrefilet, salat, bearnaise, peppersaus og pommes frites',
@@ -255,7 +263,7 @@ export class AppComponent implements OnInit {
       src: 'assets/steak.png',
     },
     {
-      price: '310,-',
+      price: '335,-',
       category: 'Kjøtt',
       title:
         'Biff A La Rosa - okse indrefilet, salat, pommes frites, rødvinssaus med champignons og bacon',
@@ -281,231 +289,231 @@ export class AppComponent implements OnInit {
     {
       price: 'Se pizza meny',
       category: 'Barn',
-      title: 'Barne pizza - velg fra pizzamenyen, avslag kr 30,-',
+      title: 'Barne pizza - velg fra pizzamenyen, avslag kr 40,-',
       allergies: '1, 7, 9, 10',
       src: 'assets/avatar.png',
     },
   ];
   Pizza = [
     {
-      price: '110/200/220,-',
+      price: '120/220/245,-',
       category: 'Pizza',
       title: 'Margerita - tomatsaus og ost',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '120/210/230,-',
+      price: '135/225/245,-',
       category: 'Pizza',
       title: 'Vesuvio - skinke',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '120/210/230,-',
+      price: '135/225/245,-',
       category: 'Pizza',
       title: 'Capricossa - skinke og champignons',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '120/210/230,-',
+      price: '135/225/245,-',
       category: 'Pizza',
       title: 'Hawaii - skinke og ananas',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '120/210/230,-',
+      price: '135/222/245,-',
       category: 'Pizza',
       title: 'Ado - pepperoni, ananas og løk',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '135/240/260,-',
+      price: '149/265/285,-',
       category: 'Pizza',
       title: 'Mama Mia - skinke, kjøttdeig og løk',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '135/240/260,-',
+      price: '149/265/285,-',
       category: 'Pizza',
       title: 'Aylish - pepperoni, kjøttdeig og løk',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '135/240/260,-',
+      price: '149/265/285,-',
       category: 'Pizza',
       title: 'Santana - skinke, kjøttdeig, løk og paprika',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '135/240/260,-',
+      price: '149/265/285,-',
       category: 'Pizza',
       title: 'Alamos - kylling, skinke og ananas',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '135/240/260-,',
+      price: '149/265/285-,',
       category: 'Pizza',
       title: 'Vegetar - champignon, paprika, løk, tomat og oliven',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Bello - pepperoni, salami, bacon og sterk kjøttdeig',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Montana - skinke, pepperoni, salami og bacon',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '149/265/285,-',
       category: 'Pizza',
       title: 'Pompeii - skinke, champignon, bacon og løk',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Herkules - skinke, pepperoni, bacon, kjøttdeig og løk',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Arena - biff, løk og bearnaise',
       allergies: '1, 3, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Bambino - biff, pepperoni, champignon, løk og bearnaise',
       allergies: '1, 3, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: "Flisa pizza'n - biff, pepperoni, kjøttdeig, skinke og mais",
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Matador - biff, pepperoni og skinke',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Tor - biff, bacon, skinke og kylling',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Mexicali - biff, skinke, bacon, løk og paprika',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Tinini - biff, skinke og ananas',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Odin - biff, skinke, bacon, pepperoni og løk',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Santos - biff, pepperoni, skinke, løk og mais',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: "Fleicher'n - biff, pepperoni og løk",
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Ciao Caio - biff, champignon, løk, paprika, tomat og bearnaise',
       allergies: '1, 3, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '135/240/260,-',
+      price: '149/265/285,-',
       category: 'Pizza',
       title: 'Våler - skinke, pepperoni, løk og mais',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '135/240/260,-',
+      price: '149/265/285,-',
       category: 'Pizza',
       title: 'Åsnes spesial - skinke, bacon og champignon',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '135/240/260,-',
+      price: '149/265/285,-',
       category: 'Pizza',
       title: 'Granlund - kylling, ananas og karri',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '135/240/260,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Kjellmyra - biff, bacon og kebab',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '120,-',
+      price: '135,-',
       category: 'Pizza',
       title: 'Calzone (innbakt) med skinke',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '135,-',
+      price: '149,-',
       category: 'Pizza',
       title: 'Calzone spesial (innbakt) med skinke, bacon og løk',
       allergies: '1, 7, 9, 10',
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title:
         'Kebabpizza - kebabkjøtt, isbergsalat, agurk, tomat, løk og kebabsaus',
@@ -513,7 +521,7 @@ export class AppComponent implements OnInit {
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title:
         'Mexicana - tacokjøtt, løk, hvitløk, jalapenos, tacokrydder og tacosaus',
@@ -521,7 +529,7 @@ export class AppComponent implements OnInit {
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title:
         'Acapulco - biff, champignon, løk, hvitløk, tacokrydder, tacosaus og jalapenos',
@@ -529,7 +537,7 @@ export class AppComponent implements OnInit {
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title:
         'Amigo - biff, kjøttdeig, løk, paprika, jalapenos, tacokrydder og tacosaus',
@@ -537,7 +545,7 @@ export class AppComponent implements OnInit {
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title:
         'Kaktus - tacokjøtt, paprika, nachos, jalapenos, tacokrydder og tacosaus',
@@ -545,7 +553,7 @@ export class AppComponent implements OnInit {
       src: 'assets/pizza.png',
     },
     {
-      price: '155/250/280,-',
+      price: '170/275/299,-',
       category: 'Pizza',
       title: 'Velg selv - velg inntil 5 ingredienser',
       allergies: 'Ingen',
@@ -554,21 +562,21 @@ export class AppComponent implements OnInit {
   ];
   Dessert = [
     {
-      price: '95,-',
+      price: '105,-',
       category: 'Dessert',
       title: 'Iskrem - 3 kuler vaniljeis, krem og sjokoladesaus',
       allergies: '3, 7',
       src: 'assets/cake.png',
     },
     {
-      price: '115,-',
+      price: '125,-',
       category: 'Dessert',
       title: 'Eplekake',
       allergies: '1, 3, 7',
       src: 'assets/cake.png',
     },
     {
-      price: '115,-',
+      price: '125,-',
       category: 'Dessert',
       title: 'Sjokoladefondant',
       allergies: '1, 3, 7',
